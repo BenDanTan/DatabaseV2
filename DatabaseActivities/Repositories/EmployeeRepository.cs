4af76ae6fc01998543de5c65c563252034608a34
Tyler Tran <19trant@elmbrookstudents.org>
2019-04-05 11:21:03 -0500

    Employee Activities

---
+++ DatabaseActivities/Repositories/EmployeeRepository.cs
@@ -0,0 +1,42 @@
+﻿using DatabaseActivities.Models;
+using DatabaseActivities.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseActivities.Repositories
+{
+    public class EmployeeRepository
+    {
+        private ApplicationDbContext dbContext;
+
+        public EmployeeRepository()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return dbContext.Employees.ToList();
+        }
+
+        public void addEmployee(Employee toAdd)
+        {
+            dbContext.Employees.Add(toAdd);
+            dbContext.SaveChanges();
+        }
+
+        public Employee getEmployeeById(int id)
+        {
+            return dbContext.Employees.Find(id);
+        }
+
+        public void DeleteEmployee(Employee toDelete)
+        {
+            dbContext.Employees.Remove(toDelete);
+            dbContext.SaveChanges();
+        }
+
+    }
+}
--- DatabaseActivities/Service/EmployeeService.cs
+++ DatabaseActivities/Service/EmployeeService.cs
@@ -1,4 +1,5 @@
 ﻿using DatabaseActivities.Models.Entity;
+using DatabaseActivities.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,31 +9,45 @@ namespace DatabaseActivities.Service
 {
     public class EmployeeService
     {
+        private EmployeeRepository suppository;
+
+        public EmployeeService()
+        {
+            suppository = new EmployeeRepository();
+        }
+
         public List<Employee> GetAllEmployees()
         {
-            return null;
+            return suppository.GetAllEmployees();
         }
 
         public Employee GetEmployeeById(int id)
         {
-            return null;
+            return suppository.getEmployeeById(id);
         }
 
         public void AddEmployee(Employee toAdd)
         {
-            
+            suppository.addEmployee(toAdd);
         }
 
         public void DeleteEmployee(Employee toDelete)
         {
-
+            suppository.DeleteEmployee(toDelete);
         }
 
         public List<Employee> GetEmployeeByWorkLength(int yearsWorked)
         {
             List<Employee> filteredList = GetAllEmployees();
-            //Filter list here
-            return filteredList;
+            List<Employee> loyalWorkers = new List<Employee>();
+            DateTime now = DateTime.Today;
+            int currentYear = now.Year;
+            foreach (Employee worker in filteredList) {
+                if (currentYear - worker.hireDate.Year >= yearsWorked) {
+                    loyalWorkers.Add(worker);
+                }
+            }
+            return loyalWorkers;
         }
     }
 }
